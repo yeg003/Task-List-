695fdb8f009b575573a10ebd6e99bc93f9a8e46a
yeg003 <yeg003@gmail.com>
2019-06-12 20:38:51 -0400

    Added task items to cards

--- app.js
+++ app.js
@@ -20,5 +20,25 @@ function addTask(e) {
         alert('Add a task');
     }
 
-    e.preventDefault();
+    // Create li element
+    const li = document.createElement('li')
+    // Add class
+    li.className = 'collection-item'
+    //Create text node and append to li 
+    li.appendChild(document.createTextNode(taskInput.value))
+    // Create new link element
+    const link = document.createElement('a')
+    // Add class
+    link.className = 'delete-item secondary-content'
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>'
+    // Append the link to li
+    li.appendChild(link)
+    // Append li to ul 
+    taskList.appendChild(li)
+    // Clear input 
+    taskInput.value = ''
+
+    e.preventDefault()
 }
+
