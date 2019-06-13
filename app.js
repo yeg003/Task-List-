4979394e502ef70a699a2f2e59ec8f1ed9ae2a9b
yeg003 <yeg003@gmail.com>
2019-06-13 13:35:41 -0400

    Completed filter tasks function

--- app.js
+++ app.js
@@ -65,5 +65,14 @@ function clearTasks() {
 // Filter tasks function 
 function filterTasks(e) {
     const text = e.target.value.toLowerCase()
-    console.log(text)
+
+    document.querySelectorAll('.collection-item').forEach
+        (function (task) {
+            const item = task.firstChild.textContent
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block'
+            } else {
+                task.style.display = 'none'
+            }
+        })
 }
